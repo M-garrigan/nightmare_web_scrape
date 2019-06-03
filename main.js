7758cf68ddc33e2f67e52e9516674d18e5fb7cc8
M-garrigan <mgarrigan99@gmail.com>
2019-06-03 19:52:31 -0400

    Changed the way nightmare js searches for the news items.
    Before I was going to duckduckgo.com and entering the search value
    manually. Now I am just going straight to the url I want and grabbing
    all the items (as a nodelist that is converted into a standard js array)
    and iterating over the first five news stories in order to pull the
    relevant data out. I then return an array of objects, where each object
    contains the news data for one news story.
    
    Also I have added the adm-zip library so that I can zip the .json file
    of data and send it to my S3 bucket.
    
    And lastly I have added some inline TODO items.

--- main.js
+++ main.js
@@ -3,9 +3,11 @@
 const binaryPack = require('./lib/bootstrap/nightmare-lambda-pack');
 
 // WIP: should be `require('xvfb')`
-const Xvfb       = require('./lib/bootstrap/xvfb'); 
+const Xvfb = require('./lib/bootstrap/xvfb'); 
 
 const Nightmare  = require('nightmare');
+const fs = require('fs');
+const AdmZip = require('adm-zip');
 
 const isOnLambda = binaryPack.isRunningOnLambdaEnvironment;
 
@@ -30,76 +32,84 @@ exports.handler = function(event, context){
         xvfb.stop((err) => context.done(err, result));
     }
 
-    // ...
     // Main logic with call to done() upon completion or error
-    // ...
 
     const nightmare = Nightmare({
       show: false,                   // show actual browser window as Nightmare clicks through
       electronPath: electronPath    // you MUST specify electron path which you receive from installation
     });
 
-        nightmare
-          .goto('https://duckduckgo.com')
-          .type('#search_form_input_homepage', 'apex legends')
-          .click('#search_button_homepage')
-          .wait('.module--news__items')
-          .evaluate( () => {
-            const parseNews = limit => {
-              let nodeArr = [];
-            
-              if (limit === 0) return nodeArr;
-              else {
-            
-                for (let i = 1; i <= limit; i += 1) {
-                  let img = null;
-                  if (document.querySelector(`.module--news__items :nth-child(${i}) .module--news__image-wrapper .module--news__image`)) {
-                    img = document.querySelector(`.module--news__items :nth-child(${i}) .module--news__image-wrapper .module--news__image`).getAttribute('style');
-                    // split string by '.'
-                    let split = img.split('.');
-                    split.shift();
-                    img = split.join('.');
-                  }
-                  
-                  let body = document.querySelector(`.module--news__items :nth-child(${i}) .module--news__body a`).innerText;
-                  let source = document.querySelector(`.module--news__items :nth-child(${i}) .module--news__footer .module--news__more-at .module--news__source`).innerText;
-                  let sourceHref = document.querySelector(`.module--news__items :nth-child(${i}) .module--news__body a`).getAttribute('href');
-                  let time = document.querySelector(`.module--news__items :nth-child(${i}) .module--news__footer .tile__time`).innerText;
-              
-                  nodeArr.push({
-                    img: img,
-                    body: body,
-                    source: source,
-                    sourceHref, sourceHref,
-                    time: time
-                  });
-                }
-              }
-              return nodeArr
+    nightmare
+      .goto('https://duckduckgo.com/?q=apex+legends&t=h_&iar=news&ia=news')
+      .wait('.js-vertical-results') // posibly change this to a 1000 ms delay
+      .evaluate( () => {
+        const newsItems = [...document.querySelectorAll('.result')]; // convert the Nodelist to an array
+        let upperLimit = 5; // we only want this number of results
+
+        if (newsItems.length < upperLimit) {
+          upperLimit = len;
+        }
+
+        if (upperLimit === 0) {
+          return [];
+        } else {
+          let data = [];
+
+          for (let i = 0; i < upperLimit; i += 1) {
+            let title = newsItems[i].querySelector('.result__body .result__title .result__a').innerText;
+            let body = newsItems[i].querySelector('.result__body .result__snippet').innerText;
+            let source = newsItems[i].querySelector('.result__body .result__extras .result__extras__url .result__url').innerText;
+            let sourceHref = newsItems[i].getAttribute('data-link');
+            let time = newsItems[i].querySelector('.result__body .result__extras .result__extras__url .result__timestamp').innerText;
+
+            let img = null;
+            if (newsItems[i].querySelector('.result__body .result__body__img')) {
+              img = newsItems[i].querySelector('.result__body .result__body__img').getAttribute('style');
+        
+              let split = img.split('.');
+              split.shift();
+              img = split.join('.');
             }
 
-            let len = document.querySelector('.module--news__items').childNodes.length;
-            let upperLimit = 5;
+            data.push({ title, body, source, sourceHref, time, img });
+          }
+          return data;
+        }
+      })
+      .end()
+      .then( result => {
+        let streamKeyword ='Apex_Legends';
+        const jsonFileName = `${streamKeyword}.json`;
 
-            if (len < 5) {
-              upperLimit = len;
-            }
-           
-            return parseNews(upperLimit);
-          })
-          .end()
-          .then( result => {
-            // json stringify data
-            // write data to file
-            // send file to s3 bucket
-            // erase file (locally)
-              
-            done(null, result);  // done() instead of context.done()
-          })
-          .catch( error => {
-              console.error('Search failed:', error);
-              done(error);         // done() instead of context.done()
-          });    
+        // json stringify data
+        let jsonResult = JSON.stringify(result);
+
+        // make .json file to hold data
+        fs.writeFileSync(jsonFileName, jsonResult);
+
+        // __dirname ==  /Users/mac/Apps/nightmare_web_scrape
+
+        // zip file
+        const zip = new AdmZip();
+        zip.addLocalFile(`${__dirname}/${jsonFileName}`);
+        zip.writeZip(`${__dirname}/${streamKeyword}.zip`)
+            
+        // TODO
+        // 1) make a name for the file (timestamp??)
+        // 2) dynamic S3 folder with a file in it acting as a summary page
+        //  Check item for timeout and if not found return nothing and do not generate a file to zip
+        // 5) send file to s3 bucket
+        // 6) erase file (locally)
+        // 7) Fill out README.md file so that it is useful
+        // 8) MAYBE:: a function to search out any old files to be erased
+        // 9) Make this lambda consume data from a SQS stream
+          
+        done(null, jsonResult);  // done() instead of context.done()
+      })
+      .catch( error => {
+          console.error('Search failed:', error);
+          done(error);         // done() instead of context.done()
+      });    
 
     });
 };
