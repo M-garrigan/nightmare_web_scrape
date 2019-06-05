50c0404f6f6159f94b9b83a9ca3f8907f6d18e7c
M-garrigan <mgarrigan99@gmail.com>
2019-06-05 05:21:39 -0400

    A helper file to return two objects.
    Each object is configuration details for entering a file into the
    'nightmare-web-bucket' s3 storage. One object is for the metadata.json
    file and the other is for the .zip files that contain the actual news
    data.

--- helpers/config_mock.js
+++ helpers/config_mock.js
@@ -1,4 +1,7 @@
 
+// 1) Rename this file to config.js
+// 2) Change Id and Key to your S3 credentials
+
 module.exports = {
   accessKeyID: "your_access_id_here",
   secretAccessKey: "your_access_key_here"
